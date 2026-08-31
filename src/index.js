21a8ab08239b5865041379e38f9ef2f0215da5da
ArnaudBay <arnaudbayale@gmail.com>
2026-08-31 10:38:29 +0100

    2nd commit

--- src/index.js
+++ src/index.js
@@ -6,29 +6,38 @@ const app = express();
 app.use(express.json());
 
 const schemaUtilisateur = z.object({
-  nom: z.string().min(2, 'Nom trop court (2 caractères min)'),
-  email: z.email('Email invalide'),
-  age: z.number().int().min(18, 'Vous devez avoir au moins 18 ans')
+    nom: z.string().min(2, 'Nom trop court (2 caractères min)'),
+    email: z.email('Email invalide'),
+    age: z.number().int().min(18, 'Vous devez avoir au moins 18 ans')
 });
 
 app.post('/utilisateurs', (req, res) => {
 
-  const resultat = schemaUtilisateur.safeParse(req.body);
+    try {
+        const resultat = schemaUtilisateur.safeParse(req.body);
 
-  if (!resultat.success) {
-    return res.status(400).json({
-      erreurs: resultat.error.issues
-    });
-  }
+        if (!resultat.success) {
+            return res.status(400).json({
+                erreurs: resultat.error.issues
+            });
+        }
 
-  const utilisateur = resultat.data;
+        const utilisateur = resultat.data;
 
-  res.status(201).json({
-    message: 'Utilisateur créé',
-    utilisateur
-  });
+        return res.status(201).json({
+            message: 'Utilisateur créé',
+            utilisateur
+        });
+
+    } catch (erreur) {
+        console.error(erreur);
+
+        return res.status(500).json({
+            message: 'Erreur interne du serveur'
+        });
+    }
 });
 
 app.listen(3000, () => {
-  console.log('Serveur démarré sur http://localhost:3000');
+    console.log('Serveur démarré sur http://localhost:3000');
 });
